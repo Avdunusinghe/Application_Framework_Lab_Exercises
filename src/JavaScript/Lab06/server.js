be6e8b4844170f11f80a17b6772ffcccfb3190f8
Avdunusinghe <avdunusinghe@gmail.com>
2022-04-27 16:22:05 +0530

    Koa Api Configured

--- src/JavaScript/Lab06/server.js
+++ src/JavaScript/Lab06/server.js
@@ -1,2 +1,15 @@
 const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const homeRouter = require("./routes/home.router");
+
 const app = new Koa();
+app.use(bodyParser());
+
+app.use(homeRouter.routes()).use(homeRouter.allowedMethods());
+
+app.use((ctx) => {
+  ctx.body = "Hello World";
+});
+
+app.listen(3000);
+console.log("Application is running on port 3000");
